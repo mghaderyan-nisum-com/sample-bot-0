ebf710e409a1878236c3a900a2c8676d87c29c98
Meysam <meysam.ghaderyan@macys.com>
2016-04-23 10:08:39 -0700

    post

--- server.js
+++ server.js
@@ -10,7 +10,20 @@ app.get('/webhook/', function (req, res) {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
-})
+});
+
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+    }
+  }
+  res.sendStatus(200);
+});
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
